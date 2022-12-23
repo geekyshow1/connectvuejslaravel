f69282a80116d2fed6cc734b41ff856a310cdfd5
geekyshows <geekyshows@gmail.com>
2022-12-23 19:10:54 +0530

    first commit

---
+++ vuejsfrontend/src/components/Home.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { onMounted } from "vue";
+import useStudent from "../composables/studentApi";
+const { studentData, error, getAllStudent } = useStudent();
+onMounted(getAllStudent);
+</script>
+
+<template>
+  <h1>Connect Vue JS with Laravel</h1>
+  <div v-if="error">Oops! Error encountered: {{ error.message }}</div>
+  <div v-else-if="studentData">
+    <div v-for="({ id, stuname, email }, i) in studentData" :key="id">
+      <h2>
+        {{ ++i }}
+        {{ stuname }}
+        {{ email }}
+      </h2>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
